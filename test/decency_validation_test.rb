ad56aada6c94db0fb89d32f3b000fb6432cfd28d
Seamus Abshere <seamus@seamus-absheres-macbook.local>
2008-12-29 13:08:34 -0500

    Separate test coverage.

--- test/decency_validation_test.rb
+++ test/decency_validation_test.rb
@@ -1,6 +1,27 @@
 require File.expand_path('../test_helper', __FILE__)
 
-describe "DecencyValidation" do
+describe "IsDecent" do
+  before do
+    @decent = 'Hello world'
+    @indecent = 'Hello f*u*c*k*i*n*g world'
+  end
+  
+  it "should not die on empty strings" do
+    assert_nothing_raised do
+      ''.is_decent?
+    end
+  end
+  
+  it "should recognize decent strings" do
+    assert @decent.is_decent?
+  end
+  
+  it "should recognize decent strings" do
+    assert !@indecent.is_decent?
+  end
+end
+
+describe "ValidatesDecencyOf" do
   before do
     DecencyValidationTest::Initializer.setup_database
     @decent = Message.create :title => 'Hello world', :description => 'I am clean'
@@ -15,11 +36,11 @@ describe "DecencyValidation" do
     assert @decent.valid?
   end
 
-  it "should recognize decent strings" do
+  it "should allow decent strings" do
     assert @decent.valid?
   end
   
-  it "should recognize indecent strings" do
+  it "should disallow indecent strings" do
     assert !@indecent.valid?
     assert_not_nil @indecent.errors.on(:title)
     assert_not_nil @indecent.errors.on(:description)
