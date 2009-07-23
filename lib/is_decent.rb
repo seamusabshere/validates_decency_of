8daa08a0ef1fa00c9147a71983ed0d60ac66bd6c
Seamus Abshere <seamus@abshere.net>
2009-07-22 20:33:11 -0400

    relax the rules to allow "planet. It's" and "bush. It". I guess it's not just Scunthorpe after all.

--- lib/is_decent.rb
+++ lib/is_decent.rb
@@ -1,7 +1,7 @@
 class String
   INDECENT_WORDS = %w(shit piss fuck cunt cocksucker motherfucker tits)
   def is_decent?
-    essence = self.downcase.gsub(/[^a-zA-Z]/, '')
+    essence = self.downcase.gsub(/[^\.\'a-zA-Z]/, '')
     INDECENT_WORDS.detect { |c| essence.include? c }.nil?
   end
 end
